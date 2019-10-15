24e62e668167448a21bd6374f36aacb6739897ea
Hung Che <chedaihung4864@gmail.com>
2019-10-14 19:17:42 -0500

    Update clock.ino

--- clock/clock.ino
+++ clock/clock.ino
@@ -10,6 +10,7 @@
  * #define IR_USE_TIMER1    tx = pin 9
  * //#define IR_USE_TIMER2    // tx = pin 3
  */
+#include <stdlib.h>
 RTC_DS1307 rtc;
 LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
 const int buzzerPin = 10;
@@ -21,7 +22,9 @@ long num[10] = {0xFF6897, 0xFF30CF, 0xFF18E7, 0xFF7A85, 0xFF10EF, 0xFF38C7, 0xFF
 int a = 0;
 int b = 0;
 int c = 0;
+int d = 0;
 int setAlarm = 0;
+
 
 void setup() {
   lcd.begin(16, 2);
@@ -43,69 +46,62 @@ void loop() {
     lcd.print(':');
     if(now.second()<10)lcd.print("0");
     lcd.print(now.second());
-    lcd.print("     ");
+    lcd.print("       ");
     getAlarm();
-    snooze();
-}
-void snooze(){
-  while(digitalRead(buzzerPin)==HIGH){
-     if(digitalRead(snoozePin)==HIGH){
-        noTone(buzzerPin);
-     }
-  }
 }
 void getAlarm(){
+  int ques1 = rand()%100+1;
+  int ques2 = rand()%100+1; 
+  int ans = ques1 + ques2;
   if (irrecv.decode(&results)){
     Serial.println(results.value, HEX);
     irrecv.resume();
     if(results.value==0xFFB04F){
         setAlarm=1;
-      delay(300);
     }else if(results.value==0xFFFFFFFF){
       setAlarm = setAlarm;  
     }
   }
-    while(setAlarm==1){
-      lcd.setCursor(0,0);
-      lcd.print("Set Hour");
-      lcd.setCursor(0,1);
-      if(irrecv.decode(&results)){
-        for(int i = 0; i<10; i++){
-          if(results.value==num[i]){
-            a = a*10+i;
-          }else if(results.value==0xFFFFFFFF){
-            a = a;  
-          }
-          irrecv.resume();
+  if(digitalRead(buzzerPin)==HIGH){
+    setAlarm=5;  
+  }
+  while(setAlarm==1){
+    lcd.setCursor(0,0);
+    lcd.print("Set Hour");
+    lcd.setCursor(0,1);
+    if(irrecv.decode(&results)){
+      for(int i = 0; i<10; i++){
+        if(results.value==num[i]){
+          a = a*10+i;
+        }else if(results.value==0xFFFFFFFF){
+          a = a;  
         }
-        if(a<24){
-          lcd.print(a);
-        }else{
-          lcd.clear();
-          lcd.print("Invalid");
-          if(irrecv.decode(&results)){
-            lcd.clear();
-            setAlarm=0;
-            a=0;
-          }
-        }
+        irrecv.resume();
+      }
+      if(a<24){
+        lcd.print(a);
         if(results.value==0xFFB04F){
           setAlarm=2;
         }
+      }else{
+        lcd.clear();
+        setAlarm = 1;
+        a=0;
       }
     }
-    while(setAlarm==2){
-      lcd.setCursor(0,0);
-      lcd.print("Set Minute");
-      lcd.setCursor(0,1);
-      if(irrecv.decode(&results)){
+  }
+  while(setAlarm==2){
+    lcd.setCursor(0,0);
+    lcd.print("Set Minute");
+    lcd.setCursor(0,1);
+    if(irrecv.decode(&results)){
       for(int i = 0; i<10; i++){
         if(results.value==num[i]){
           b = b*10+i;
         }else if(results.value==0xFFFFFFFF){
           b = b;  
         }else if(results.value==0xFFB04F){
-            setAlarm++;
+          setAlarm++;
         }
         irrecv.resume();
       }
@@ -113,76 +109,101 @@ void getAlarm(){
       lcd.print(':');
       if(b<60){
         lcd.print(b);
-      }else{
-        lcd.clear();
-        lcd.print("Invalid");
-        if(irrecv.decode(&results)){
-          lcd.clear();
-          setAlarm=1;
-          b=0;
-        }
-      }
-      if(results.value==0xFFB04F){
+        if(results.value==0xFFB04F){
           setAlarm=3;
         }
+      }else{
+        lcd.clear();
+        setAlarm = 2;
+        b=0;
       }
     }
-    while(setAlarm==3){
-      lcd.setCursor(0,0);
-      lcd.print("Set Second");
-      lcd.setCursor(0,1);
-      if(irrecv.decode(&results)){
-        for(int i = 0; i<10; i++){
-          if(results.value==num[i]){
-           c = c*10+i;
-          }else if(results.value==0xFFFFFFFF){
-            c = c;  
-          }else if(results.value==0xFFB04F){
-            setAlarm++;
-          }
-          irrecv.resume();
+  }
+  while(setAlarm==3){
+    lcd.setCursor(0,0);
+    lcd.print("Set Second");
+    lcd.setCursor(0,1);
+    if(irrecv.decode(&results)){
+      for(int i = 0; i<10; i++){
+        if(results.value==num[i]){
+         c = c*10+i;
+        }else if(results.value==0xFFFFFFFF){
+          c = c;  
+        }else if(results.value==0xFFB04F){
+          setAlarm++;
         }
-        lcd.print(a);
-        lcd.print(':');
-        lcd.print(b);
-        lcd.print(':');
-        if(c<60){
-          lcd.print(c);
-        }else{
-          lcd.clear();
-          lcd.print("Invalid");
-          if(irrecv.decode(&results)){
-            lcd.clear();
-            setAlarm=2;
-            c=0;
-          }
-        }
+        irrecv.resume();
+      }
+      lcd.print(a);
+      lcd.print(':');
+      lcd.print(b);
+      lcd.print(':');
+      if(c<60){
         lcd.print(c);
         if(results.value==0xFFB04F){
           setAlarm=4;
         }
-      }
-    }
-    while(setAlarm==4){
-      lcd.setCursor(0,1);
-      lcd.print("Alarm-");
-      if(a<10)lcd.print("0");
-      lcd.print(a);
-      lcd.print(':');
-      if(b<10)lcd.print("0");
-      lcd.print(b);
-      lcd.print(':');
-      if(c<10)lcd.print("0");
-      lcd.print(c);
-      if(results.value==0xFFB04F){
-        setAlarm=0;
-      }
-    }
-    DateTime now = rtc.now();
-    if(now.hour()==a && now.minute()==b && now.second()==c){
-      tone(buzzerPin, 100);
-      a=0;
-      b=0;
-      c=0;
+      }else{
+        lcd.clear();
+        setAlarm = 3;
+        c=0;
+      }        
     }
   }
+  while(setAlarm==4){
+    lcd.setCursor(0,1);
+    lcd.print("Alarm-");
+    if(a<10)lcd.print("0");
+    lcd.print(a);
+    lcd.print(':');
+    if(b<10)lcd.print("0");
+    lcd.print(b);
+    lcd.print(':');
+    if(c<10)lcd.print("0");
+    lcd.print(c);
+    if(results.value==0xFFB04F){
+      setAlarm=0;
+    }
+  }
+  DateTime now = rtc.now();
+  if(now.hour()==a && now.minute()==b && now.second()==c){
+    tone(buzzerPin, 100);
+    a=0;
+    b=0;
+    c=0;
+  }
+  if(irrecv.decode(&results)){
+    if(results.value==0xFFA25D){
+      a=0;
+      b=0;
+      c=0; 
+      lcd.setCursor(0,1);
+      lcd.clear();
+    }
+  }
+  while(setAlarm==5){
+    lcd.setCursor(0,0);
+    lcd.print(ques1);
+    lcd.print('+');
+    lcd.print(ques2);
+    lcd.print("      ");
+    if(irrecv.decode(&results)){
+      for(int i = 0; i<10; i++){
+        if(results.value==num[i]){
+          d = d*10+i;
+        }else if(results.value==0xFFA25D){
+          d=0;
+        }
+        irrecv.resume();
+      }
+      lcd.print(d);
+    }
+    if(digitalRead(snoozePin)==HIGH&&d==ans){
+      noTone(buzzerPin);
+      setAlarm=0;
+      lcd.setCursor(0,1);
+      lcd.clear();
+      d=0;
+    }
+  }
+}
